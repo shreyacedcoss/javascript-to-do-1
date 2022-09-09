a201479b34e3b3e5726e42d0d83b36812bd0d936
shreya <shreya@cedcoss.com>
2022-09-09 15:14:57 +0530

    task1

---
+++ script.js
@@ -0,0 +1,29 @@
+
+  
+
+//     var arr = new Array(4);
+  
+//  document.write("Creating 2D array <br>");
+  
+
+// for (var i = 0; i < gfg.length; i++) {
+// arr[i] = new Array(2);
+// }
+  
+// var h = 0;
+  
+
+// for (var i = 0; i < 2; i++) {
+//     for (var j = 0; j < 2; j++) {
+//         arr[i][j] = h++;
+//     }
+// }
+  
+
+// for (var i = 0; i < 2; i++) {
+//     for (var j = 0; j < 2; j++)    {
+//         document.write(arr[i][j] + " ");
+//     }
+//     document.write("<br>");
+// } 
+                
